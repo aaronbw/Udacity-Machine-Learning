e1e3cc76ba85a9e72e653e065950eb47ecb064c6
Your-Full-Name <aaronbw.js@gmail.com>
2019-11-07 11:19:15 -0500

    Coursework complete through Ch 10 Text Learning

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -1,17 +1,13 @@
-#!/usr/bin/python3
-
 """ 
     skeleton code for k-means clustering mini-project
 
 """
 
-
-
-
 import pickle
 import numpy
 import matplotlib.pyplot as plt
 import sys
+import pandas as pd
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
@@ -40,7 +36,7 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 
 
 ### load in the dict of dicts containing all the data on each person in the dataset
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "rb") )
+data_dict = pickle.load( open("../final_project/final_project_dataset_unix.pkl", "rb") )
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
@@ -49,39 +45,90 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+total_payments = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, total_payments]
 data = featureFormat(data_dict, features_list )
+
+
+# Alternative #1 to get min and max from github evykassirer (DOES NOT WORK WELL)
+from sklearn import preprocessing
+
+min_max_scaler = preprocessing.MinMaxScaler()
+data = min_max_scaler.fit_transform(data)
+
+print(min_max_scaler.data_min_)
+print(min_max_scaler.data_max_)
+
 poi, finance_features = targetFeatureSplit( data )
+
+# Alternative #2 to get min and max from github jeremy-shannon (WORKS WELL)
+salMin = 10000000
+salMax = 0
+for k in data_dict:
+    sal = data_dict[k]["salary"]
+    if sal != 'NaN':
+        if sal < salMin:
+            salMin = sal
+        if sal > salMax:
+            salMax = sal
+
+print("min:", salMin)
+print("max:", salMax)
+
+
+
+df = pd.DataFrame(data_dict)
+df2 = pd.DataFrame(df.transpose())
+df2['exercised_stock_options'] = pd.to_numeric(df2['exercised_stock_options'], errors='coerce')
+df2['salary'] = pd.to_numeric(df2['salary'], errors='coerce')
+
+print(int(df2['exercised_stock_options'].max()))
+print(int(df2['exercised_stock_options'].min()))
+
+print(int(df2['salary'].max()))
+print(int(df2['salary'].min()))
 
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, line below assumes 2 features)
-for f1, f2 in finance_features:
+
+################################################################
+for f1, f2, _ in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
+################################################################
 
 
+# from sklearn.cluster import KMeans
+# features_list = ["poi", feature_1, feature_2]
+# data2 = featureFormat(data_dict, features_list )
+# poi, finance_features = targetFeatureSplit( data2 )
+# clf = KMeans(n_clusters=2)
+# pred = clf.fit_predict( finance_features )
+# Draw(pred, finance_features, poi, name="clusters_before_scaling.pdf", f1_name=feature_1, f2_name=feature_2)
 
+#################################################################
 from sklearn.cluster import KMeans
-features_list = ["poi", feature_1, feature_2]
-data2 = featureFormat(data_dict, features_list )
-poi, finance_features = targetFeatureSplit( data2 )
-clf = KMeans(n_clusters=2)
-pred = clf.fit_predict( finance_features )
-Draw(pred, finance_features, poi, name="clusters_before_scaling.pdf", f1_name=feature_1, f2_name=feature_2)
+clf = KMeans(2)
+clf.fit(finance_features)
+pred = clf.predict(finance_features)
+#################################################################
 
+### rename the "name" parameter when you change the number of features
+### so that the figure gets saved to a different file
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+#################################################################
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters-x.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print("no predictions object named pred found, no clusters to plot")
-
+################################################################
 
 
 
--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -40,23 +40,33 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
+        # temp_counter += 1
+        # if temp_counter < 200:
+        path = os.path.join('..', path[:-1])
+        print(path)
+        email = open(path, "r")
 
-            ### use parseOutText to extract the text from the opened email
+        ### use parseOutText to extract the text from the opened email
+        words = parseOutText(email)
+        
+        ### use str.replace() to remove any instances of the words
+        ### ["sara", "shackleton", "chris", "germani"]
+        list_rep  = ["sara", "shackleton", "chris", "germani"]
+        for x in list_rep:
+            words = words.replace(x,"")
+            
+        ### append the text to word_data
+        word_data.append(words)
+        
+        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        if name == "sara":
+            from_data.append(0)
+        elif name == "chris":
+            from_data.append(1)
+        else:
+            from_data.append(X)
 
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
-
-            ### append the text to word_data
-
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
-
-            email.close()
+        email.close()
 
 print("emails processed")
 from_sara.close()
@@ -66,9 +76,14 @@ pickle.dump( word_data, open("your_word_data.pkl", "wb") )
 pickle.dump( from_data, open("your_email_authors.pkl", "wb") )
 
 
-
+print(word_data[152])
 
 
 ### in Part 4, do TfIdf vectorization here
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+vectorizer = TfidfVectorizer(stop_words="english",lowercase=True)
+vectorizer.fit_transform(word_data)
 
+print(len(vectorizer.get_feature_names()))
+print(vectorizer.get_feature_names()[34597])
